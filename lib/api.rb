9096d253f43248d41d025c12cf166ad179bd2c4f
MasayaNishijima <25636356+MasayaNishijima@users.noreply.github.com>
2023-11-03 18:22:42 +0900

    トークンの処理をクラスに切り出し

--- lib/api.rb
+++ lib/api.rb
@@ -1,21 +1,5 @@
-require 'dotenv'
-Dotenv.load
+require './token'
 
-require 'net/http'
-require 'json'
+token = Token.new
 
-uri = URI.parse('https://identity.tyo1.conoha.io/v2.0/tokens')
-https = Net::HTTP.new(uri.host, uri.port)
-https.use_ssl = true
-
-parms = { 'auth' => {
-  'passwordCredentials' => { 'username' => ENV['CONOHA_API_USER_ID'],
-                             'password' => ENV['CONOHA_API_USER_PASSWORD'] },
-  'tenantId' => ENV['CONOHA_API_TENANT_ID']
-} }
-headers = { "Accept" => "application/json" }
-
-response = https.post(uri.path, parms.to_json, headers)
-
-p response
-p response.body
+p token
