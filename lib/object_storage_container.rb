680644d9f2661227f95d5c425407490fc32ac491
MasayaNishijima <25636356+MasayaNishijima@users.noreply.github.com>
2023-11-26 16:49:55 +0900

    オブジェクト削除、コンテナ削除を実装

--- lib/object_storage_container.rb
+++ lib/object_storage_container.rb
@@ -34,8 +34,8 @@ class ObjectStorageContainer
 		JSON.parse(response.body)
 	end
 
-	def put_object(directory: '.', file_name:)
-		uri = URI.parse("https://object-storage.tyo1.conoha.io/v1/nc_#{@token.tenant_id}/#{@container_name}/#{file_name}")
+	def put_object(directory: '.', file_name:, object_name: file_name)
+		uri = URI.parse("https://object-storage.tyo1.conoha.io/v1/nc_#{@token.tenant_id}/#{@container_name}/#{object_name}")
 		https = Net::HTTP.new(uri.host, uri.port)
 		https.use_ssl = true
 
@@ -47,13 +47,13 @@ class ObjectStorageContainer
 				request.body_stream = file
 				request.content_length = file.size
 				response = https.request(request)
+				raise StandardError, "failed to put object. response code: #{response.code}" unless response.code == '201'
 			end
 		end
-		raise StandardError, "failed to put object. response code: #{response.code}" unless response.code == '201'
 	end
 
-	def get_object(file_name:)
-		uri = URI.parse("https://object-storage.tyo1.conoha.io/v1/nc_#{@token.tenant_id}/#{@container_name}/#{file_name}")
+	def get_object(object_name:)
+		uri = URI.parse("https://object-storage.tyo1.conoha.io/v1/nc_#{@token.tenant_id}/#{@container_name}/#{object_name}")
 		https = Net::HTTP.new(uri.host, uri.port)
 		https.use_ssl = true
 
@@ -64,6 +64,35 @@ class ObjectStorageContainer
 		raise StandardError, "failed to get object. response code: #{response.code}" unless response.code == '200'
 
 		response.body
+	end
+
+	def delete_object(object_name:)
+		uri = URI.parse("https://object-storage.tyo1.conoha.io/v1/nc_#{@token.tenant_id}/#{@container_name}/#{object_name}")
+		https = Net::HTTP.new(uri.host, uri.port)
+		https.use_ssl = true
+
+		headers = { 'Accept' => 'application/json', 'X-Auth-Token' => @token.id }
+
+		response = https.delete(uri.path, headers)
+
+		raise StandardError, "failed to delete object. response code: #{response.code}" unless response.code == '204'
+	end
+
+	def delete_container()
+		# curl -i -X DELETE \
+		# -H "Accept: application/json" \
+		# -H "X-Auth-Token: 2c6f2de2126a4102b38368c32e7043db" \
+		# https://object-storage.tyo1.conoha.io/v1/nc_cc54f7476b8e444bad238a943a94ccdf/container
+
+		uri = URI.parse("https://object-storage.tyo1.conoha.io/v1/nc_#{@token.tenant_id}/#{@container_name}")
+		https = Net::HTTP.new(uri.host, uri.port)
+		https.use_ssl = true
+
+		headers = { 'Accept' => 'application/json', 'X-Auth-Token' => @token.id }
+
+		response = https.delete(uri.path, headers)
+
+		raise StandardError, "failed to delete container. response code: #{response.code}" unless response.code == '204'
 	end
 
 	private
