120fc1d2f9b22991d2d218b5dce442641e5a126c
MasayaNishijima <25636356+MasayaNishijima@users.noreply.github.com>
2024-08-17 14:14:36 +0900

    イメージの一覧取得処理を実装

---
+++ lib/image.rb
@@ -0,0 +1,26 @@
+require 'net/http'
+require 'json'
+
+# ボリュームの情報取得・制御するためのクラス()
+class Image
+  def initialize(token:)
+    @token = token
+  end
+
+  def self.images_info(token:)
+    uri = URI.parse('https://image-service.c3j1.conoha.io/v2/images')
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+
+    headers = { 'Accept' => 'application/json', 'X-Auth-Token' => token.to_s }
+
+    response = https.get(uri.path, headers)
+    raise StandardError, "failed to get images info. response code: #{response.code}" unless response.code == '200'
+
+    JSON.parse(response.body)['images']
+  end
+
+  def self.images_names(token:)
+    images_info(token: token).map { |image| image['name'] }
+  end
+end
--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,13 @@
 require './lib/token'
 require './lib/server'
 require './lib/object_storage_container'
+require './lib/image'
 
 token = Token.new
 
 puts "起動中のサーバー台数: #{Server.count(token: token)}"
 puts "オブジェクトストレージのコンテナ情報: #{ObjectStorageContainer.containers_info(token: token)}"
+p Image.images_names(token: token)
 
 # container = ObjectStorageContainer.new(token: token, container_name: 'test', new: false)
 # puts "#{container.container_name}のオブジェクト一覧: #{container.get_container_objects_info}"
